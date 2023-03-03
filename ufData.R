6a1b855cbd994953a765308fb44f6d74d45b8117
Tom-B-Rogers <tomrogers04@gmail.com>
2023-03-03 18:51:50 +1100

    Additional updates

--- ufData.R
+++ ufData.R
@@ -3,16 +3,16 @@ library(fitzRoy)
 
 ## --------- download and write data to csv
 
-seasons <- seq(2011, 2022)
+seasons <- seq(2012, 2022)
 
 seasonData <- c()
 
-for (i in seasons){
-  seasonData[[i]] <- fetch_player_stats(season = i, source = "AFL")
-}
-
-data <- seasonData[!sapply(seasonData, is.null)]
-data <- data %>% reduce(full_join)
+# for (i in seasons){
+#   seasonData[[i]] <- fetch_player_stats(season = i, source = "AFL")
+# }
+# 
+# data <- seasonData[!sapply(seasonData, is.null)]
+#data <- data %>% reduce(full_join)
 
 playerData <- fetch_player_details()
 
@@ -21,3 +21,5 @@ ids <- unique(playerData$id)
 fileName <- "fullUF2022.csv"
 
 write.csv(data, fileName)
+
+#data %>% filter(as.Date(data$utcStartTime) > '2022-01-01') %>% write.csv(., "2022.csv")
